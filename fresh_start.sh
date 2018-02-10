3d32303a2fe2a2274f0cbbcc7a4ffa3d760f07bd
Robert Curtin <robertecurtin@gmail.com>
2018-02-10 15:34:10 -0500

    Cleaned up fresh start script, checks weren't working anyway

--- fresh_start.sh
+++ fresh_start.sh
@@ -5,16 +5,6 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 ./copy_aliases.sh
-
-if ! [ -x "$(command -v vim)" ]; then
-  ./install_vim.sh
-fi
-
-if ! [ -x "$(command -v atom)" ]; then
-  ./install_atom.sh
-fi
-
-if ! [ -x "$(command -v lenv)" ]; then
-  echo "Lenv"
-  ./install_lua.sh
-fi
+./install_vim.sh
+./install_atom.sh
+./install_lua.sh
